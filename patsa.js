1ff1a8ecbedf082f0c067baf3be42069b4e029ce
weirdoa <47795005+weirdoa@users.noreply.github.com>
2019-03-23 20:43:45 -0400

    Update patsa.js

--- patsa.js
+++ patsa.js
@@ -30,5 +30,18 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
